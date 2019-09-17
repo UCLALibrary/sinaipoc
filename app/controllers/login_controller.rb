f7af43d25f56f550b2efd07f576fa51d11965e7a
darrowco <github_03@darrow.com>
2019-09-17 14:40:08 -0700

    Cal-771 Validate token on Callback
    modified:   app/controllers/login_controller.rb
    new file:   app/controllers/static_controller.rb
    modified:   app/views/login/new.html.erb
    new file:   app/views/static/index.html.erb
    modified:   config/routes.rb

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,11 +2,22 @@ require 'securerandom'
 
 class LoginController < ApplicationController
   def new
-    @token = SecureRandom.uuid
-    @login = Login.create(token: @token)
+    if params[:token].present?
+      validate_token
+    else
+      @token = SecureRandom.uuid
+      @login = Login.create(token: @token)
+      @token_success = "Token created!"
+    end
   end
 
-  def mock_sinai
-    puts params
+  def validate_token
+    if Login.find_by(token: params[:token])
+      flash[:success] = 'You can now see the images!'
+      redirect_to '/'
+    else
+      flash[:error] = 'Your session has expired'
+      redirect_to '/login'
+    end
   end
 end
