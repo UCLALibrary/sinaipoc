a7686411b707fbdfafbbb3d2452bdc5c6112cb7f
darrowco <github_03@darrow.com>
2019-09-17 14:40:08 -0700

    partial token check and compare

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,11 +2,37 @@ require 'securerandom'
 
 class LoginController < ApplicationController
   def new
-    @token = SecureRandom.uuid
-    @login = Login.create(token: @token)
+    if params[:token].present?
+      @tsuccess="I see the token: #{params[:token]}"
+      validate_token
+    else
+      @token = SecureRandom.uuid
+      @login = Login.create(token: @token)
+      @tsuccess="No token"
+    end
   end
 
   def mock_sinai
     puts params
   end
+
+  def login_response
+  end
+
+  def validate_token
+    # comparing token from GET to tokens in db
+    users_token=params[:token]
+    if Login.find_by(token: params[:token])
+      @dbreturn="hurray"
+      redirect_to '/'
+      flash[:success]='you can now see the images'
+    else
+      @dbreturn="Your sessions has expired - please click on the <em>Login</em> button"
+      redirect_to '/login'
+      #flash[:success]='something funny'
+    end
+  end
 end
+
+
+###     @tsuccess="I see the token: #{params[:token]}" if params[:token].present? 
