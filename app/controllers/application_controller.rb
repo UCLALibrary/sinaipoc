4ecfd10c84b894bea2cf221a11ef89e9be2afb54
jendiamond <thejendiamond@gmail.com>
2019-09-24 16:35:28 -0700

    Add callback URL
    We added the callback URL that the User is coming from
    so when EMEL redirects them
    (after they get the token and the cookie)
    they will be redirected to the page they wanted to see
    (not just the home page of Sinai)
    
    ---
    
    Changes to be committed:
    modified:   app/controllers/application_controller.rb
    modified:   app/controllers/login_controller.rb
    modified:   app/views/layouts/_header.html.erb
    modified:   app/views/login/new.html.erb
    modified:   app/views/static/index.html.erb
    new file:   config/initializers/session_store.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,21 @@ class ApplicationController < ActionController::Base
   def sinai_authenticated?
     @path_check = ''
     @cookie_created = ''
+    
+    # Checks to see if we are on the Login page and do nothing
     if request.fullpath.include?(login_path)
       @path_check = '@path_check request.fullpath.include?(login_path)'
     else
+      @popsicle = 'Popsicle'
       if has_cookie?
+        # do nothing
         'has_cookie You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
       elsif has_token?
         # user has a token so we then need to set the cookie based on the fact that they have a token in the database
         set_cookie
         'has_token You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
       else
-        redirect_to '/login'
+        redirect_to "/login?callback=#{request.fullpath}"
       end
     end
   end
@@ -25,9 +29,10 @@ class ApplicationController < ActionController::Base
   end
 
   def set_cookie
+
     cookies[:sinai_authenticated] = {
       value: true,
-      expires: 1.year.from_now,
+      expires: Time.now + 90.days,
       domain: ENV['DOMAIN']
     }
     @cookie_created = 'Cookie created'
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,10 @@ class LoginController < ApplicationController
   before_action :create_token
 
   def new
+    @domain = request.domain
+    @port = request.port
+    @requested_path = params[:callback]
+    @full_path = "http://#{@domain}:#{@port}#{@requested_path}"
   end
 
   def create_token
