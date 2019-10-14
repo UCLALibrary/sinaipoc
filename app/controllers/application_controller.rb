ce7eae84ccba1b7d8818dcba8557aefa419e4d50
darrowco <github_03@darrow.com>
2019-10-14 11:38:51 -0700

    updated cookies: domain to library.ucla.edu and expire to 90 days

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,11 @@ class ApplicationController < ActionController::Base
   end
 
   def set_iv_cookie
-    cookies[:initialization_vector] = { value: @iv }
+    cookies[:initialization_vector] = {
+      value: @iv,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
   end
 
   def create_encrypted_string
