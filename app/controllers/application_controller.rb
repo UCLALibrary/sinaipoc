b4c15ce9730e38d7d8fe49d1f49e05ed4cf96e95
darrowco <github_03@darrow.com>
2019-11-01 11:31:14 -0700

    added tests; view debug cookies after successful EMEL authentication; see notes at start of application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
+### TEST VERSION
+###   localhost:3030
+###   click button
+###   log into EMEL
+###   the home page will display
+###     Sinai Digital Library - Proof of Concept
+###   view debug cookies A-G
+###     debug cookies A-F show packed, unpacked and repacked values are shown for the two SinaiPOC cookies
+###       the packed and repacked values should match
+###     debug cookie G shows the unencrypted authorization string
+###       the unecrypted authetication string should display
+###
+###   e.g.
+###   A_TEST_iv_already_packed   %24%7Cw%CA%B6%ED%9F%7B6%96%EF%D5z%EB%5D6
+###   B_TEST_iv_unPacked         247C77CAB6ED9F7B3696EFD57AEB5D36
+###   C_TEST_iv_rePacked         %24%7Cw%CA%B6%ED%9F%7B6%96%EF%D5z%EB%5D6
+###   D_TEST_auth_already_packed %D1%FF%14%F2%DBd%BA%85M%B8%91%A6%9B%FC%81%1C-%DC%0B%DE%DAI%EF%C0%05%0F%5D%89%28Kt9
+###   E_TEST_auth_unpacked       D1FF14F2DB64BA854DB891A69BFC811C2DDC0BDEDA49EFC0050F5D89284B7439
+###   F_TEST_auth_RePacked       %D1%FF%14%F2%DBd%BA%85M%B8%91%A6%9B%FC%81%1C-%DC%0B%DE%DAI%EF%C0%05%0F%5D%89%28Kt9
+###   G_TEST_unencrypted         Authenticated+2019-11-01
+
 # frozen_string_literal: true
 require 'openssl'
 
@@ -16,14 +37,21 @@ class ApplicationController < ActionController::Base
         'has_cookie You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
       elsif has_token?
         # user has a token so we then need to set the cookie based on the fact that they have a token in the database
-        set_session_cookie
         set_auth_cookie
         set_iv_cookie
         'has_token You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
+        test_iv_cookie
+        test_auth_cookie
+        test_encrypted_string
       else
         redirect_to "/login?callback=#{request.original_url}"
       end
     end
+  end
+
+  def has_cookie?
+    # Does user have the sinai cookie?
+    @has_cookie = cookies[:sinai_authenticated]
   end
 
   def has_token?
@@ -38,23 +66,19 @@ class ApplicationController < ActionController::Base
   def set_auth_cookie
     @encryptd_str = create_encrypted_string
     cookies[:sinai_authenticated] = {
-      value: @cipher_text,
+      value: @cipher_text_unPacked,
       expires: Time.now + 90.days,
       domain: ENV['DOMAIN']
     }
   end
 
   def set_iv_cookie
+    @iv_unPacked = @iv.unpack('H*')[0].upcase
     cookies[:initialization_vector] = {
-    value: @iv,
-    expires: Time.now + 90.days,
-    domain: ENV['DOMAIN']
-  }
-end
-
-  def has_cookie?
-    # Does user have the sinai cookie set to true?
-    @has_cookie = session[:sinai_authenticated_test]
+      value: @iv_unPacked,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
   end
 
   def create_encrypted_string
@@ -62,24 +86,67 @@ end
     cipher = OpenSSL::Cipher::AES256.new :CBC
     cipher.encrypt
     @iv = cipher.random_iv
-    @iv = "abcdefghijklmnop"
     cipher.key = ENV['CIPHER_KEY']
     cipher.iv = @iv
-    @cipher_text = cipher.update("Authenticated #{todays_date}") + cipher.final
-    @cipher_text = @cipher_text.unpack('H*')[0].upcase
+    @cipher_text_packed = cipher.update("Authenticated #{todays_date}") + cipher.final
+    @cipher_text_unPacked = @cipher_text_packed.unpack('H*')[0].upcase
   end
 end
 
-# Share with Kevin/Hardy
+def test_iv_cookie
+  cookies[:A_TEST_iv_already_packed] = {
+    value: @iv,
+    expires: Time.now + 90.days,
+    domain: ENV['DOMAIN']
+  }
+  @test_iv_unPacked = cookies[:initialization_vector]
+  cookies[:B_TEST_iv_unPacked] = {
+    value: @test_iv_unPacked,
+    expires: Time.now + 90.days,
+    domain: ENV['DOMAIN']
+  }
+  @test_iv_rePacked = [@test_iv_unPacked].pack('H*').unpack('C*').pack('c*')
+  cookies[:C_TEST_iv_rePacked] = {
+    value: @test_iv_rePacked,
+    expires: Time.now + 90.days,
+    domain: ENV['DOMAIN']
+  }
 
-# https://ruby-doc.org/stdlib-2.4.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-random_key
-# https://medium.com/@Bakku1505/playing-with-symmetric-encryption-algorithms-in-ruby-8652f105341e
+  def test_auth_cookie
+    cookies[:D_TEST_auth_already_packed] = {
+      value: @cipher_text_packed,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
+    @cipher_text_unPacked = cookies[:sinai_authenticated]
+    cookies[:E_TEST_auth_unpacked] = {
+      value: @cipher_text_unPacked,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
+    @authRePacked = [@cipher_text_unPacked].pack('H*').unpack('C*').pack('c*')
+    cookies[:F_TEST_auth_RePacked] = {
+      value: @authRePacked,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
+  end
 
-# def decrypt_string
-#   decipher = OpenSSL::Cipher::AES256.new :CBC
-#   decipher.decrypt
-#   decipher.iv = ENV['CIPHER_INITIALIZATION_VECTOR']
-#   decipher.key = ENV['CIPHER_KEY']
-#   decipher.update(@cipher_text) + decipher.final # @cipher_text
-# end
-# CIPHER_KEY
+  def test_encrypted_string
+    decipher = OpenSSL::Cipher::AES256.new :CBC
+    decipher.decrypt
+    @test_iv_unPacked = cookies[:initialization_vector]
+    @test_iv_rePacked = [@test_iv_unPacked].pack('H*').unpack('C*').pack('c*')
+    decipher.iv = @test_iv_rePacked
+    decipher.key = ENV['CIPHER_KEY']
+    @test_auth_unPacked = cookies[:sinai_authenticated]
+    test_auth_rePacked = [@test_auth_unPacked].pack('H*').unpack('C*').pack('c*')
+    @test_unencrypted = decipher.update(test_auth_rePacked) + decipher.final
+
+    cookies[:G_TEST_unencrypted] = {
+      value: @test_unencrypted,
+      expires: Time.now + 90.days,
+      domain: ENV['DOMAIN']
+    }
+  end
+end
